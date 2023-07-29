59b9851a6d18c6e48140ec1548803bd0dc4e3c20
unknown <hebaalhwai96@gmail.com>
2023-07-29 20:40:12 +0300

    add labs

---
+++ Jqeury/lab2/students.js
@@ -0,0 +1,22 @@
+[
+    {
+        "ID": 1,
+        "studentName": "heba",
+        "grade": 90
+    },
+    {
+        "ID": 2,
+        "studentName": "sarah",
+        "grade": 80
+    },
+    {
+        "ID": 3,
+        "studentName": "hoda",
+        "grade": 90
+    },
+    {
+        "ID": 4,
+        "studentName": "mona",
+        "grade": 100
+    }
+]
